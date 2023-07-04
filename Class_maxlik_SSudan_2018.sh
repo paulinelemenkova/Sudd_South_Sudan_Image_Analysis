d92e6b5b3529d4d5ab204d42e71c52a93a98dcde
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2023-07-04 10:16:25 +0200

    Add files via upload

---
+++ Class_maxlik_SSudan_2018.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+# Import data
+# importing the image subset with 7 Landsat bands and display the raster map
+r.import input=/Users/polinalemenkova/grassdata/SSudan/LC08_L2SP_173055_20180201_20200902_02_T1_SR_B1.TIF output=L8_2018_01 resample=bilinear extent=region resolution=region --overwrite
+r.import input=/Users/polinalemenkova/grassdata/SSudan/LC08_L2SP_173055_20180201_20200902_02_T1_SR_B2.TIF output=L8_2018_02 extent=region resolution=region
+r.import input=/Users/polinalemenkova/grassdata/SSudan/LC08_L2SP_173055_20180201_20200902_02_T1_SR_B3.TIF output=L8_2018_03 extent=region resolution=region
+r.import input=/Users/polinalemenkova/grassdata/SSudan/LC08_L2SP_173055_20180201_20200902_02_T1_SR_B4.TIF output=L8_2018_04 extent=region resolution=region
+r.import input=/Users/polinalemenkova/grassdata/SSudan/LC08_L2SP_173055_20180201_20200902_02_T1_SR_B5.TIF output=L8_2018_05 extent=region resolution=region
+r.import input=/Users/polinalemenkova/grassdata/SSudan/LC08_L2SP_173055_20180201_20200902_02_T1_SR_B6.TIF output=L8_2018_06 extent=region resolution=region
+r.import input=/Users/polinalemenkova/grassdata/SSudan/LC08_L2SP_173055_20180201_20200902_02_T1_SR_B7.TIF output=L8_2018_07 extent=region resolution=region
+#
+# listing the files
+g.list rast
+#
+# Set computational region to match the scene
+g.region raster=L8_2018_01 -p
+#
+# grouping data by i.group to store VIZ, NIR, MIR into group/subgroup (leaving out TIR)
+i.group group=L8_2018 subgroup=res_30m \
+  input=L8_2018_01,L8_2018_02,L8_2018_03,L8_2018_04,L8_2018_05,L8_2018_06,L8_2018_07
+#
+# Clustering: generating signature file and report using k-means clustering algorithm
+i.cluster group=L8_2018 subgroup=res_30m \
+  signaturefile=cluster_L8_2018 \
+  classes=10 reportfile=rep_clust_L8_2018.txt --overwrite
+#
+# Classification by i.maxlik module
+i.maxlik group=L8_2018 subgroup=res_30m \
+  signaturefile=cluster_L8_2018 \
+  output=L8_2018_cluster_classes reject=L8_2018_cluster_reject --overwrite
+#
+# Mapping
+d.mon wx0
+g.region raster=L8_2018_cluster_classes -p
+r.colors L8_2018_cluster_classes color=rainbow -e
+d.rast L8_2018_cluster_classes
+d.legend raster=L8_2018_cluster_classes title="2018" title_fontsize=12 font="Helvetica" fontsize=10 bgcolor=white border_color=white
+d.out.file output=SSudan_2018 format=jpg --overwrite
+#
+# Rejected classes
+d.mon wx1
+g.region raster=L8_2018_cluster_classes -p
+d.rast L8_2018_cluster_reject
+d.legend raster=L8_2018_cluster_reject title="2018" title_fontsize=12 font="Helvetica" fontsize=10 bgcolor=white border_color=white
+d.out.file output=SSudan_2018_reject format=jpg --overwrite
+#
+# Accuracy assessment by r.kappa: error matrix and kappa parameters of classification result.
+g.region raster=L8_2018_cluster_classes -p
+r.kappa -w classification=L8_2018_cluster_classes reference=training_classes_Sudd
+
+# export Kappa matrix as CSV file "kappa.csv"
+r.kappa classification=L8_2018_cluster_classes reference=training_classes_Sudd output=kappa.csv -m -h --overwrite
